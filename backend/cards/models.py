205a6707124ef6281564f161e2853e49ceccd1e4
caynan <caynanletcher@gmail.com>
2022-09-07 15:21:42 -0700

    set, card migrations (enable foreignkey for Card)

--- backend/cards/models.py
+++ backend/cards/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from sets.models import Set
 
 
 class Card(models.Model):
-  # set = models.ForeignKey(Set, on_delete=models.CASCADE)
+  set = models.ForeignKey(Set, related_name='cards', on_delete=models.CASCADE)
   name = models.CharField(max_length=50)
   number = models.PositiveSmallIntegerField()
   def __str__(self):
