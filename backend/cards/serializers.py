f3d340f4cd2154850d7c155c46117ca8d2480ccf
caynan <caynanletcher@gmail.com>
2022-09-16 14:05:21 -0700

    allow create() to find set by name and/or code

--- backend/cards/serializers.py
+++ backend/cards/serializers.py
@@ -1,4 +1,5 @@
 from cards.models import Card
+from sets.models import Set
 from rest_framework import serializers
 from sets.serializers import SetSerializer
 
@@ -10,3 +11,12 @@ class CardSerializer(serializers.HyperlinkedModelSerializer):
   class Meta:
     model = Card
     fields = ['name', 'number', 'set']
+
+  def create(self, validated_data):
+    set_data = validated_data.pop('set')
+    set = (Set.objects.filter(code=set_data.get('code')) | Set.objects.filter(name=set_data.get('name'))).first()
+    if not set:
+      raise serializers.ValidationError({
+        'set': 'Set not found.'
+      })
+    return Card.objects.create(set=set, **validated_data)
--- backend/sets/serializers.py
+++ backend/sets/serializers.py
@@ -1,8 +1,8 @@
+import re
 from sets.models import Set
 from rest_framework import serializers
 
-
-class SetSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Set
-        fields = ['name', 'code']
+# SetSerializer cannot be a ModelSerializer or HyperlinkedModelSerializer, as name and code need to be allowed blank 
+class SetSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100, allow_blank=True, required=False)
+    code = serializers.CharField(max_length=20, allow_blank=True, required=False)
