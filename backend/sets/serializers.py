ef9d15f8a27792e6756f34801ac86f1ff6ebbab0
caynan <caynanletcher@gmail.com>
2022-09-19 13:52:05 -0700

    import sets from pokemontcgapi

--- backend/sets/serializers.py
+++ backend/sets/serializers.py
@@ -1,4 +1,3 @@
-import re
 from sets.models import Set
 from rest_framework import serializers
 
@@ -6,3 +5,15 @@ from rest_framework import serializers
 class SetSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100, allow_blank=True, required=False)
     code = serializers.CharField(max_length=20, allow_blank=True, required=False)
+
+    def create(self, validated_data):
+        return Set.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.save()
+        return instance
+
+    def delete(self, validated_data):
+        return Set.objects.delete(**validated_data)
---
+++ import_sets.py
@@ -0,0 +1,24 @@
+import requests
+from pathlib import Path
+import environ
+
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env('.env')
+username = env("ADMIN_USER")
+password = env("ADMIN_PASSWORD")
+
+SETS_URL = 'http://localhost:8000/sets/'
+
+total_sets = requests.get('https://api.pokemontcg.io/v2/sets').json()
+saved_sets = requests.get(SETS_URL, auth=(username, password)).json()
+
+for set in total_sets['data']:
+  # if set['name'] == saved_sets[i]['name']:
+  print(set['name'])
+  response = requests.post(SETS_URL, json = {
+      'name': set['name'],
+      'code': set['id'],
+    }, auth=(username, password))
+  print(response)
