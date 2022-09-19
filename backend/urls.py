be52423ff511d1f2b7a7453e8fa48b4e0011fcaa
caynan <caynanletcher@gmail.com>
2022-09-19 13:11:28 -0700

    connect admin panel to project

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
+from django.contrib import admin
 from rest_framework import routers
 from cards import views as cards_views
 from sets import views as sets_views
+
 
 router = routers.DefaultRouter()
 router.register(r'cards', cards_views.CardViewSet)
@@ -11,5 +13,6 @@ router.register(r'sets', sets_views.SetViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),  
 ]
