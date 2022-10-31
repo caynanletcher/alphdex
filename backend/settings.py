0d781000e30483257a709f7b6bdc926142d7dad4
caynan <caynanletcher@gmail.com>
2022-10-31 13:51:26 -0700

    increase backend results

--- backend/settings.py
+++ backend/settings.py
@@ -143,5 +143,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 100,
 }
